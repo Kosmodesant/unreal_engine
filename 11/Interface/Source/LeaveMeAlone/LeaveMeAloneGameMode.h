6dba32de8c52622748b0bb6612700a158f75d0b9
Nikita <element_90@bk.ru>
2024-02-03 22:06:00 +0300

    release 1.0

---
+++ 11/Interface/Source/LeaveMeAlone/LeaveMeAloneGameMode.h
@@ -0,0 +1,16 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LeaveMeAloneGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALeaveMeAloneGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALeaveMeAloneGameMode();
+};
